3b9784ba7ac7546defa56139f30e30a917f729b6
Junior007 <agilgomez@gmail.com>
2022-12-04 19:43:47 +0100

    en proceso

---
+++ daily.UI/Views/Controls/DailyTaskDetail.xaml.cs
@@ -0,0 +1,35 @@
+﻿using daily.UI.ViewsModel.DailyWork;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace daily.UI.Views.Controls
+{
+    public partial class DailyTaskDetail : UserControl
+    {
+
+
+        internal DailyWorkViewModel DailyWorkViewModel
+        {
+            get { return (DailyWorkViewModel)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        internal static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(DailyWorkViewModel), typeof(DailyTaskDetail), new PropertyMetadata(null, SetValue));
+
+        private static void SetValue(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DailyTaskDetail dailyTaskDetail = (DailyTaskDetail)d;
+            if (dailyTaskDetail != null)
+            {
+                dailyTaskDetail.DataContext = dailyTaskDetail.DailyWorkViewModel;
+            }
+        }
+
+        public DailyTaskDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
