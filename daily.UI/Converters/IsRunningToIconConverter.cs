fe2d9f5c9a8ba226c7b062435c6bcdc4f95b11d5
Junior007 <agilgomez@gmail.com>
2022-12-13 17:45:15 +0100

    stop/start icons

---
+++ daily.UI/Converters/IsRunningToIconConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace daily.UI.Converters
+{
+    internal class IsRunningToIconConverter : IValueConverter
+    {
+
+        string play = "\uE102";
+        string stop = "\uE769";
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRunning = (bool)value;
+            return isRunning ? stop : play;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string state = value as string;
+            return state==play;
+        }
+    }
+}
