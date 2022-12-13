46af1d7b0cb633629538c68b680d78a8d01a5bf7
Junior007 <agilgomez@gmail.com>
2022-12-13 17:45:55 +0100

    start/stop

--- daily.UI/Converters/IsRunningToIconConverter.cs
+++ daily.UI/Converters/IsRunningToIconConverter.cs
@@ -7,18 +7,18 @@ namespace daily.UI.Converters
     internal class IsRunningToIconConverter : IValueConverter
     {
 
-        string play = "\uE102";
+        string start = "\uE102";
         string stop = "\uE769";
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             bool isRunning = (bool)value;
-            return isRunning ? stop : play;
+            return isRunning ? stop : start;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string state = value as string;
-            return state==play;
+            return state==start;
         }
     }
 }
