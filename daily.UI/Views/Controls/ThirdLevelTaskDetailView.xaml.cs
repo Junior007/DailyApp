962392a7312b418b12883a934a39a2d33ef7eee5
Junior007 <agilgomez@gmail.com>
2022-12-15 11:31:26 +0100

    Control para el tercer nivel

---
+++ daily.UI/Views/Controls/ThirdLevelTaskDetailView.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using daily.UI.ViewsModel;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace daily.UI.Views.Controls
+{
+    public partial class ThirdLevelTaskDetailView : UserControl
+    {
+        public ThirdLevelTaskDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
